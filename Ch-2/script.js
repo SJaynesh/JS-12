da221e6dc121b7064ab5eff09d795c0f35a8b35e
SJaynesh <jayneshsodvadiya@gmail.com>
2025-06-26 13:17:50 +0530

    JS-Print

---
+++ Ch-2/script.js
@@ -0,0 +1,10 @@
+console.log("Hello JavaScript");
+console.log("Hello Programming");
+
+document.write("<h2>Hello World</h2> <hr/>");
+document.write(
+  "<img src= 'https://m.media-amazon.com/images/S/pv-target-images/59846ef06faaa50337f9c2fb21a65bcf6fc0c060b5c1dc4028ff0a95801415c6.jpg'>"
+);
+
+// alert("Hiiii");
+// window.alert("Hello World");
